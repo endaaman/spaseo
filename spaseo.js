47c0bdbf79f4128af710962cfb8f4c2382322f35
endaaman <buhibuhidog@gmail.com>
2015-06-24 04:08:56 +0900

    remove des for coffee

--- spaseo.js
+++ spaseo.js
@@ -1,6 +1,5 @@
 if ((typeof window !== 'undefined' && window !== null)) {
     module.exports = require('./client');
 } else if ((typeof global !== 'undefined' && global !== null)) {
-    require('coffee-script/register');
     module.exports = require('./server');
 }
