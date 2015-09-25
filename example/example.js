2f55ec592d7fa3c2f841b92701953e27cd370c59
endaaman <buhibuhidog@gmail.com>
2015-09-25 18:23:32 +0900

    update example

--- example/example.js
+++ example/example.js
@@ -1,41 +1,48 @@
 angular.module('app', [
-    'ngRoute'
+  'ngRoute'
 ])
 .config(function($locationProvider) {
-    $locationProvider
-    .html5Mode(true)
-    .hashPrefix('!');
+  $locationProvider
+  .html5Mode(true)
+  .hashPrefix('!');
 })
 .config(function($routeProvider) {
-    $routeProvider
-    .when('/', {
-        template: '<p>{{message}}</p>',
-        controller: 'HomeCtrl'
-    })
-    .when('/other', {
-        template: '<ul><li ng-repeat="user in users">{{user.name}}: {{user.message}}</li></ul>',
-        controller: 'OtherCtrl'
-    })
-    .otherwise({
-        redirectTo: '/'
-    });
+  $routeProvider
+  .when('/', {
+    template: '<p>{{message}}</p>',
+    controller: 'HomeCtrl'
+  })
+  .when('/users', {
+    template: '<ul><li ng-repeat="user in users">{{user.name}}: {{user.message}}</li></ul>',
+    controller: 'UsersCtrl'
+  })
+  .when('/:path', {
+    template: '<h1>404</h1>',
+    controller: '404Ctrl'
+  });
 })
 .controller('HomeCtrl', function($scope, $timeout){
-    var cb = spaseo();
-    $scope.message = 'this should not be rendered..'
-    $timeout(function() {
-        $scope.message = 'this should be rendered!!'
-        cb();
-    }, 2000);
+  var cb = spaseo();
+  $scope.message = 'this should not be rendered..'
+  $timeout(function() {
+    $scope.message = 'this should be rendered!!'
+    cb();
+  }, 2000);
 })
-.controller('OtherCtrl', function($scope, $http){
-    var cb = spaseo();
-    $http.get('/users.json')
-    .success(function(data) {
-        $scope.users = data;
-        cb()
-    });
+.controller('UsersCtrl', function($scope, $http){
+  var cb = spaseo();
+  $http.get('/users.json')
+  .success(function(data) {
+    $scope.users = data;
+    cb()
+  });
+})
+.controller('404Ctrl', function($scope){
+  spaseo()(404);
 })
 .run(function($rootScope, $location){
     $rootScope.$location = $location;
+    spaseo.wrap(function (cb){
+      $rootScope.$evalAsync(cb);
+    })
 });
--- example/start.sh
+++ example/start.sh
@@ -10,13 +10,15 @@ echo $prefix'Welcome to example of spaseo.js'
 echo $prefix'Now started servers'
 echo $prefix'nginx : http://localhost:8080'
 echo $prefix'spaseo: http://localhost:9999'
+echo $prefix
 echo $prefix'See spaseo log'
 echo $prefix'$ less +F example/spaseo.log'
-echo $prefix'check rendered result'
+echo $prefix
+echo $prefix'Check rendered result'
 echo $prefix'$ curl http://localhost:8080'
 echo $prefix'$ curl http://localhost:8080?_escaped_fragment_'
 echo $prefix'$ curl http://localhost:8080 -A googlebot'
-echo $prefix'Check rendered result'
+echo $prefix
 echo $prefix'To stop this type `Ctrl+C`..'
 
 wait `jobs -l %2| awk '{print $2}'`
